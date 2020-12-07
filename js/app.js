80d37c9b77fdfc26fe62d5b15176c3e649b6601f
Opilane TTHK <opilane@tthk.ee>
2020-12-07 13:43:09 +0200

    Commit

--- js/app.js
+++ js/app.js
@@ -9,9 +9,10 @@ button.addEventListener('click', (event) =>{
     const value = selected.options[selected.selectedIndex].value;
     console.log(value);
 
-    let BitcoinsValue = bitcoins.value;
+    let BitcoinsValue = parseInt(bitcoins.value);
+    console.log(typeof(BitcoinsValue));
 
-    let url = `https://api.coindesk.com/v1/bpi/currentprice/eur.json`;
+    let url = `https://api.coindesk.com/v1/bpi/currentprice/EUR.json`;
     console.log(url);
 
     fetch(url)
@@ -19,13 +20,19 @@ button.addEventListener('click', (event) =>{
         return response.json();
     })
     .then(data => {
-        console.log(data.bpi.EUR);
-
-        /*value = `${code.value}` ;
-        total+= `${rate.value}`;
-        totalSpan.innerHTML = total;
-        let text = `BitCoins: ${BitcoinsValue}; Currency: ${value}; Description: ${description.value}; Rate: ${rate.value}`;
-        document.createTextNode(text);*/
+        if(value == "EUR")
+        {
+        console.log(data.bpi.EUR.description);
+        let equation = parseFloat(data.bpi.EUR.rate_float) * BitcoinsValue;
+        totalSpan.innerHTML = equation + ` ${data.bpi.EUR.description}`;
+        document.querySelector('.calculation').innerHTML = `BitCoins: ${BitcoinsValue} <br> Currency: ${value} <br> Description: ${data.bpi.EUR.description} <br> ${value} Rate: ${data.bpi.EUR.rate_float} <br> Value in ${value}: ${equation}`;
+        }
+        if(value == "USD"){
+            console.log(data.bpi.USD.description);
+        let equation = parseFloat(data.bpi.USD.rate_float) * BitcoinsValue;
+        totalSpan.innerHTML = equation + ` ${data.bpi.USD.description}`;
+        document.querySelector('.calculation').innerHTML = `BitCoins: ${BitcoinsValue} <br> Currency: ${value} <br> Description: ${data.bpi.USD.description} <br> ${value} Rate: ${data.bpi.USD.rate_float} <br> Value in ${value}: ${equation}`;
+        }
     })
     .catch(error => {
         console.log(error);
